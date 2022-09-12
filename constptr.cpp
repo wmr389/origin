e07f6241091b54b6149535a377fb77a4b7e76223
wmr_Xiaosheng <389922594@qq.com>
2022-09-12 12:38:20 +0800

    add cpp

---
+++ constptr.cpp
@@ -0,0 +1,16 @@
+////
+//// Created by lenovo on 2022/9/1.
+////
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    int a = 10;
+//    int b = 20;
+//    const int *aa = &a;//指针常量
+//    int const *aaa = &a;//指针常量
+//    int *const p = &a;
+//    cout << b << endl;
+//    return 0;
+//}
